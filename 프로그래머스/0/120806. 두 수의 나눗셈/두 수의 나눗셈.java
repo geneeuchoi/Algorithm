7115c188815597990f075ee330d2a8463cee038a
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-04-29 11:11:33 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.06 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int num1, int num2) {
-        double result = num1 / num2;
-        int nam = num1 % num2;
+        int num3 = num1 % num2;
+        double num4 = num1 / num2;
         for (int i = 1; i < 4; i++) {
-            if (nam != 0) {
-                num1 = nam * 10;
-                result += num1 / num2 * Math.pow(0.1, i);
-                nam = num1 % num2;
-            } else {
-                break;
-            }
+            if (num3 != 0) {
+             num3 = num3 * 10 / num2;
+             double num5 = num4 + num3 * Math.pow(0.1, i);
+            }   
         }
-        int returnResult = (int)Math.round(result*1000);
-        return returnResult;
+         
+        int result = num5 * 1000;
+        return result;
     }
 }

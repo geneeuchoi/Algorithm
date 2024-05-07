9aab8d9c5684f7618adab60ebcda97833b6244db
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-07 09:19:10 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for (int x = 1; x < n; x ++){
+        if (n % x == 1) {
+            answer = x;
+            break;
+        }
+    }
+    return answer;
+}

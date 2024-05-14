98874ab3a6e77f64b6081863868bd62ad42eea8f
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-14 09:40:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(long num) {
+        int cnt = 0;
+        while(num != 1 && cnt < 500) {
+            if (num % 2 ==0) {
+                num /= 2;
+            } else{
+                num = num * 3 + 1;
+            }
+            cnt++;
+            }
+        return (cnt >= 500) ? -1 : cnt;
+    }
+}

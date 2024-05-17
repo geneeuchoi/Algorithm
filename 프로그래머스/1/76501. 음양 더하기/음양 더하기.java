2318e5e482fd0aae44fe27491a88146dd7344679
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-17 15:01:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,12 +1,9 @@
 class Solution {
     public int solution(int[] absolute, boolean[] signs) {
         int answer = 0;
+        
         for(int i = 0; i < absolute.length; i++) {
-            if(signs[i] == true) {
-                answer = answer + absolute[i];
-            } else {
-                answer = answer - absolute[i];
-            }
+            answer = signs[i] == true ? answer + absolute[i] : answer - absolute[i];
         }
         
         return answer;

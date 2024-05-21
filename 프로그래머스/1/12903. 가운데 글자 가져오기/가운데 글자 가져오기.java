daa7b71e04447028b33802b785e3ec03ba097981
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-21 09:24:43 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,14 +1,26 @@
 class Solution {
     public String solution(String s) {
-        char[] charArr = s.toCharArray();
-        int charArrSize = charArr.length;
-        String answer = "";
-        switch(charArrSize%2){
-                case 0 -> 
-                    answer = String.valueOf(charArr[charArrSize/2-1]) + String.valueOf(charArr[charArrSize/2]);
-                case 1 -> 
-                    answer = String.valueOf(charArr[charArrSize/2]);
+        int sLength = s.length();
+        String s1 = "";
+        if (sLength%2 == 0) {
+            //짝수라면
+            s1 = s.substring(sLength/2-1, sLength/2+1);
+        } else {
+            //홀수라면
+            s1 = s.substring(sLength/2, sLength/2+1);
         }
-        return answer;
+        return s1;
+        
+        
+        // char[] charArr = s.toCharArray();
+        // int charArrSize = charArr.length;
+        // String answer = "";
+        // switch(charArrSize%2){
+        //         case 0 -> 
+        //             answer = String.valueOf(charArr[charArrSize/2-1]) + String.valueOf(charArr[charArrSize/2]);
+        //         case 1 -> 
+        //             answer = String.valueOf(charArr[charArrSize/2]);
+        // }
+        // return answer;
     }
 }

c897ee422d587df0dfe5c415ee3a729b2311ce07
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-25 11:23:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,12 +1,12 @@
-	class Solution {
-  public String solution(int n) {
-    String answer = "";
- 
-    for(int i=1; i<=n; i++) {
-      if(i%2 != 0) answer += "수";
-      else answer += "박";
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder(); 
+        for(int i = 0 ; i < n; i++) {
+          switch(i % 2) {
+              case 0 -> sb.append("수");
+              case 1 -> sb.append("박");
+            }
+        }
+    return sb.toString();
     }
- 
-    return answer;
-  }
 }

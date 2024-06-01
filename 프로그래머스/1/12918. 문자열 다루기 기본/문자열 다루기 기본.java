0153f032a417f30a86556f2ac47d6939016f7488
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-06-02 04:21:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6) {
+            return  false;
+        }
+        for(char x : s.toCharArray()) {
+            if (!Character.isDigit(x)) {
+                answer = false;
+                break;
+            } 
+        }
+        return answer;
+    }
+}

664226589060295677b36bb17dda8573cc3f678c
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-07-01 10:46:29 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.18 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -5,45 +5,37 @@ class Solution {
         int[] answer = new int[targets.length];
         HashMap<Character, Integer> map = new HashMap<>();
         
-        // keymap 위 모든 알파벳의 최소 클릭 수 구하기
-        for(int i=0;i<keymap.length;i++){
-
+        for(String key : keymap) {
             
-            // 최소 클릭수로 덮어씌우기 위해 문자열 끝부터 시작
-            for(int j=0;j<keymap[i].length();j++){
+            for(int i = 0; i < key.length(); i++) {
                 
+                if (map.containsKey(key.charAt(i))) {
                 
-                if (map.containsKey(keymap[i].charAt(j))) {
-                    // 최소 클릭 수가 뒤에 나왔을 때 갱신
-                    Integer nCurrCnt = map.get(keymap[i].charAt(j));
-                    if (nCurrCnt > (j+1)){
-                        map.put(keymap[i].charAt(j),j+1);
+                    Integer currentCnt = map.get(key.charAt(i));
+                    
+                    if(currentCnt> i+1) {
+                      map.put(key.charAt(i), i+1);   
                     }
-                }
-                else {
-                    map.put(keymap[i].charAt(j),j+1);
-                }
-                
+                } else {
+                    map.put(key.charAt(i), i+1);
+                } 
             }
         }
         
+        System.out.println(map);
         
-        // 모든 targets 단어에 대해 반복
-        for(int i=0;i<targets.length;i++){
-            int nCnt = 0;
-            for(int j=0;j<targets[i].length();j++){
-                char alpha = (targets[i].charAt(j));
-                Integer nMinClick = map.get(alpha);
-                if ( nMinClick != null ) {
-                    nCnt += nMinClick;
-                }
-                else{
-                    nCnt = -1;  // i번째 단어는 작성할 수 없음
+        for(int t = 0; t < targets.length; t++) {
+            String target = targets[t];
+            int min = 0;
+            for(int k = 0; k < target.length(); k++) {
+                if(!map.containsKey(target.charAt(k))) {
+                    min = -1;
                     break;
+                } else {
+                    min += map.get(target.charAt(k));
                 }
             }
-            answer[i] = nCnt;
-            
+            answer[t] = min;
         }
         
         

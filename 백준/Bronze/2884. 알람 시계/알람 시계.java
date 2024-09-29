802c020b6f2e8f86c551478b61eee2fcca52f2eb
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-09-29 23:37:06 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18480 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        if (minute-45 < 0) {
+            minute += 60;
+            hour--;
+        }
+
+        minute -= 45;
+        if (hour < 0) hour = 23;
+        System.out.println(hour + " " + minute);
+
+
+    }
+}

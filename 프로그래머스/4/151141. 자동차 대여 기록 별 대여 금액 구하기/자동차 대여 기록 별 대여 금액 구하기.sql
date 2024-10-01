11d882b0f66e47271f0acf02a654b424de852971
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-10-01 23:58:30 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,17 +1,20 @@
-SELECT history_id,
-       round(daily_fee * (datediff(end_date, start_date) + 1) * (100 - ifnull(discount_rate, 0)) /
-             100, 0) fee
-FROM (SELECT *,
-             CASE
-                 WHEN datediff(end_date, start_date) + 1 < 7 THEN NULL
-                 WHEN datediff(end_date, start_date) + 1 < 30 THEN '7일 이상'
-                 WHEN datediff(end_date, start_date) + 1 < 90 THEN '30일 이상'
-                 ELSE '90일 이상'
-                 END duration_type
-      FROM car_rental_company_rental_history) a
-         JOIN car_rental_company_car b
-              ON a.car_id = b.car_id
-         LEFT JOIN car_rental_company_discount_plan c
-                   ON c.car_type = b.car_type AND a.duration_type = c.duration_type
-WHERE b.car_type = '트럭'
-ORDER BY 2 DESC, 1 DESC
+WITH CANT_RENTAL AS (
+    SELECT DISTINCT CAR_ID 
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE 
+    DATE_FORMAT(START_DATE, '%Y-%m') = '2022-11' OR 
+    DATE_FORMAT(END_DATE, '%Y-%m') = '2022-11' OR 
+    (START_DATE < '2022-11-01' AND END_DATE >= '2022-12-01')
+),
+DISCOUNT AS (
+    SELECT CAR_TYPE, DISCOUNT_RATE 
+    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    WHERE DURATION_TYPE	= '30일 이상'
+)
+SELECT C.CAR_ID, C.CAR_TYPE, 
+CONVERT((C.DAILY_FEE * 30) * (100 - D.DISCOUNT_RATE) / 100 , UNSIGNED) FEE
+FROM CAR_RENTAL_COMPANY_CAR C JOIN DISCOUNT D 
+ON C.CAR_TYPE = D.CAR_TYPE 
+WHERE C.CAR_ID NOT IN (SELECT * FROM CANT_RENTAL)
+HAVING FEE BETWEEN 500000 AND 1999999
+ORDER BY 3 DESC, 2 ASC , 1 DESC

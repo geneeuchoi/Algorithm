1d931acda29d40b1387c79d81090f98a2f99f1a1
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-11-05 12:42:33 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 31516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,45 @@
-T = int(input()) #테스트케이스의 개수
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+# 상하좌우 벡터
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-def BFS(x,y):           
-    queue = [(x,y)]
-    matrix[x][y] = 0 # 방문처리
+def dfs(y, x):
+    visited[y][x] = 1
+    count = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (0 <= nx < M) and (0<= ny < N):
+            if not visited[ny][nx] and graph[ny][nx]:
+                count += dfs(ny, nx)
+    return count
 
-    while queue:
-        x,y = queue.pop(0)
+# 테스트 케이스 개수
+T = int(input())
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+# 테스트 케이스 만큼 반복
+for _ in range(T):
+    # 가로 길이 M 세로 길이 N 배추 개수 K
+    M, N, K = map(int, input().split())
+    
+    # 그래프, 방문 노드 초기화
+    graph = [[0] * M for _ in range(N)] 
+    visited = [[0] * M for _ in range(N)] 
 
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                continue
+    # 그래프에 배추 개수 초기화
+    for _ in range(K):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
 
-            if matrix[nx][ny] == 1 :
-                queue.append((nx,ny))
-                matrix[nx][ny] = 0
+    # 컴포넌트 세기
+    res = []
+    for i in range(N): # 세로
+        for j in range(M): # 가로
+            if not visited[i][j] and graph[i][j]:
+                res.append(dfs(i, j))
 
-# 행렬만들기
-for i in range(T):
-    M, N, K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
-    cnt = 0
+    print(len(res))
 
-    for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
-
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a,b)
-                cnt += 1
-
-    print(cnt)

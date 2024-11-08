bd2fe6b0466d6b7ed8f0d939c2c8799aa8edbd69
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-11-09 00:09:39 +0900

    [Silver I] Title: 스타트링크, Time: 420 ms, Memory: 71808 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,23 +1,35 @@
-import sys
 from collections import deque
-
-def bfs(v):
-    q = deque([v])
-    visited[v] = 1
-    while q:
-        v = q.popleft()
-        if v == G:
-            return count[G]
-        for i in (v+U, v-D): #U만큼 위로 or D만큼 아래로
-            if 0 < i <= F and not visited[i]:
-                visited[i] = 1
-                count[i] = count[v] + 1
-                q.append(i)
-    if count[G] == 0:
-        return "use the stairs"
-
+import sys
 input = sys.stdin.readline
+#sys.setrecursionlimit(10000000)
+
+def bfs():
+    q = deque()
+    q.append(S-1)
+    visited[S - 1] = 1 # 시작점 방문 처리
+
+    while q:
+        floor = q.popleft()
+        # 이동한 층이 G와 일치할 시 
+        if floor == G-1:
+            print(visited[floor]-1) # 시작점 방문 처리해줬던 것 -1
+            return
+        # 그렇지 않을 경우
+        for j in (floor+U, floor-D):
+            if (0 <= j < F) and not visited[j]:
+                visited[j] = visited[floor] + 1
+                q.append(j)
+
+    # 엘리베이터로 방문할 수 없는 경우
+    if not visited[G-1]:
+        print("use the stairs")
+
+
+# 총 층수 F, 현재 층수 S, 스타트링크 층수 G, 위로 갈 수 있는 층수 U, 아래로 갈 수 있는 층수 D
 F, S, G, U, D = map(int, input().split())
-visited = [0 for i in range(F+1)]
-count = [0 for i in range(F+1)]
-print(bfs(S))
+
+# 1층 이상 F층 이하
+visited = [0] * F
+
+
+bfs() 

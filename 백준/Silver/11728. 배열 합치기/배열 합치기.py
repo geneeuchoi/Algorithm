67ea74554e45b7713c4307aba84a6ff5e51e8d8c
CHOIYOUJIN <100894921+geneeuchoi@users.noreply.github.com>
2025-08-02 05:10:17 +0900

    [Silver V] Title: 배열 합치기, Time: 1560 ms, Memory: 314180 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,14 +1,31 @@
-from sys import stdin
+from sys import stdin, stdout
 
-N, M = map(int, stdin.readline().strip().split(' '))
-A = list(map(int, stdin.readline().strip().split(' ')))
-B = list(map(int, stdin.readline().strip().split(' ')))
+N, M = map(int, stdin.readline().split())
+A = list(map(int, stdin.readline().split()))
+B = list(map(int, stdin.readline().split()))
 
-A.sort()
-B.sort()
+A_end, B_end = N, M
+A_start, B_start = 0, 0
 
-combined = A + B
-combined.sort()
+result = []
 
-for elem in combined:
-    print(elem, end = ' ')
+while A_start < A_end and B_start < B_end:
+    if A[A_start] == B[B_start]:
+        result.append(str(A[A_start]))
+        A_start += 1
+    elif A[A_start] < B[B_start]:
+        result.append(str(A[A_start]))
+        A_start += 1
+    else:
+        result.append(str(B[B_start]))
+        B_start += 1
+
+while A_start < A_end:
+    result.append(str(A[A_start]))
+    A_start += 1
+
+while B_start < B_end:
+    result.append(str(B[B_start]))
+    B_start += 1
+
+stdout.write(' '.join(result))

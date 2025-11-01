fa1be98833f45125bb3f5dd40ccec857701bc4e7
CHOIYOUJIN <100894921+geneeuchoi@users.noreply.github.com>
2025-11-01 14:24:41 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,14 @@
 import sys
 from sys import stdin
 
-L = int(stdin.readline().strip())
-string = stdin.readline().strip()
-hash_result = 0
-for i in range(L):
-    hash_result += (ord(string[i:i+1])-96) * (31 ** i)
+L = int(stdin.readline().rstrip())
+string = stdin.readline().rstrip()
+
+M, r = 1234567891, 31
+hash_result, power = 0, 1
+for ch in string:
+    value = ord(ch) - 96
+    hash_result = (hash_result + value * power) % M
+    power = (power * r) % M
 
 print(hash_result)

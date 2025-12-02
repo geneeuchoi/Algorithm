7253c9c8b9ed595c971b0c5579073594a68b773d
CHOIYOUJIN <100894921+geneeuchoi@users.noreply.github.com>
2025-12-02 13:55:18 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+def recursive(start, end, step, tmp):
+	if step == m: 
+		available.append(tmp)
+		return
+
+	for i in range(start, end):
+		recursive(i, end, step+1, tmp + [i])
+
+n, m = map(int, stdin.readline().rstrip().split(" "))
+available = []
+recursive(1, n+1, 0, [])
+
+for available_elem in available:
+	for elem in available_elem:
+		print(elem, end = " ")
+	print()
